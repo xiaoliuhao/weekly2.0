5ddfd0998205d54cd9c1e56f0c8f15701128b15d
Liu <470401911@qq.com>
2016-04-28 21:40:51 +0800

    add liu in group

--- application/controllers/Group.php
+++ application/controllers/Group.php
@@ -9,7 +9,6 @@
 class Group extends CI_Controller{
     function __construct(){
         parent::__construct();
-<<<<<<< HEAD
         $this->load->session();        
     }
 
@@ -24,16 +23,11 @@ class Group extends CI_Controller{
     public function delete(){
         
     }
-<<<<<<< aa7d3417735ae555ecffb89860a96d8e7b5df738
 
-=======
->>>>>>> 	modified:   application/controllers/Group.php
-=======
         
     }
 
     public function index(){
-        sadf
+        echo 'liu';
     }
->>>>>>> 9091825176660e040a9b48121e7a52999adc3df9
 }
