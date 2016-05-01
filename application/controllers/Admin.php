57a4d7c7ea0f8f36ea4503e64b52696451ef2766
Liu <470401911@qq.com>
2016-05-01 22:39:55 +0800

    add get get_admin_list,delete_admin,change_level

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -15,12 +15,37 @@ class Admin extends CI_Controller{
 
     
 
-    public function get_member_list(){
+    public function get_admin_list(){
         $this->input->get('gid');
-        $this->input->post('uid');
-        $members = $this->admin->get_member_list($gid);
+        $admins = $this->admin->get_member_list($gid);
+        var_dump($admins);
     }
 
-    
+    public function delete_admin(){
+        $g_id     = $this->inpit->post('gid');
+        $admin_id = $this->input->post('admin_id');  //当前管理员id
+        $delete_id= $this->input->post('delete_id'); //需要被删除的管理员id
+        $result   = $this->admin->delete_admin($g_id,$admin_id,$delete_id);
+        if($result == 1){
+            //删除成功
+        }else{
+            //权限不足删除失败
+        }
+    }
+
+    public function change_level(){
+        $g_id     = $this->input->post('gid');
+        $admin_id = $this->input->post('admin_id'); //当前管理员uid
+        $change_id= $this->input->post('uid');
+        $new_level= $this->input->post('level');
+
+        $result   = $this->admin->change_level($g_id,$admin_id,$change_id,$new_level);
+        if($result == 1){
+            //修改成功 将行为写入日志
+            $this->base->write_group_log($g_id,$admin_id,'change'.$change_id.'level as'.$new_level);
+        }else{
+            //权限不足修改失败
+        }
+    }
 
 }
--- application/models/Admin_Model.php
+++ application/models/Admin_Model.php
@@ -12,5 +12,41 @@ class Admin_Model extends CI_Model{
         $this->load->model('Base_Model','base');
     }
 
+    public function get_admin_list($gid){
+        $datas = $this->base->select_array('*','group_admin','g_id',$gid);
+        $i = 0;
+        foreach($datas as $data){
+            $admins[$i] = $this->base->select('*','user','uid',$data['uid']);
+            $admins[$i]['level'] = $data['level'];
+            $i++;
+        }
+        return $admins;
+    }
+
+    public function delete_admin($gid,$admin_id,$delete_id){
+        $admin_info = $this->db->select('*')->get_where($this->db->dbprefix('group_admin'),array('uid'=>$admin_id,'g_id'=>$gid));
+        $delete_info = $this->db->select('*')->get_where($this->db->dbprefix('group_admin'),array('uid'=>$delete_id,'g_id'=>$gid));
+
+        if($admin_info['level'] < $delete_info['level']){
+            //如果当前管理员权限大于需要被删除的管理员权限
+            $this->db->delete('group_admin',array('uid'=>$delete_id));
+            return 1;
+        }else{
+            return 2;   //权限不足
+        }
+    }
+    
+    public function change_level($gid,$admin_id,$change_id,$level){
+        $admin_info = $this->db->select('*')->get_where($this->db->dbprefix('group_admin'),array('uid'=>$admin_id,'g_id'=>$gid));
+        $change_info = $this->db->select('*')->get_where($this->db->dbprefix('group_admin'),array('uid'=>$change_id,'g_id'=>$gid));
+        if($admin_info['level'] < $delete_info['level']){
+            //如果当前管理员权限大于需要被删除的管理员权限
+            $this->db->update('group_admin',array('level'=>$level),array('uid'=>$delete_id));
+            return 1;
+        }else{
+            return 2;   //权限不足
+        }
+    }
+    
     
 }
